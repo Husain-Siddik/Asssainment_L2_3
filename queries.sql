d3b51e33cf5cef2e389d5483f38916f0244d3c17
Husain-Siddik <ronysiddik21@gmail.com>
2025-12-24 16:39:05 +0600

    queryes added

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
+ -- Query 1: JOIN 
+ --Retrieve booking information along with:
+
+--Customer name
+--Vehicle name
 
 SELECT
     bookings.booking_id,
@@ -12,6 +17,8 @@ JOIN users ON bookings.user_id = users.user_id
 JOIN vehicles ON bookings.vehicle_id = vehicles.vehicle_id;
 
 
+-- Query 2: EXISTS
+-- Find all vehicles that have never been booked.
 
 SELECT  *
 FROM Vehicles v
@@ -22,18 +29,21 @@ WHERE NOT EXISTS (
 );
 
 
-
+-- Query 3: WHERE 
+-- Retrieve all available vehicles of a specific type (e.g. cars).
 
 select  *
 from
   vehicles
 where
   status = 'available'
-and type  = 'car'
+and type  = 'car' ;
 
 
 
 
+--GROUP BY and HAVING 
+-- Find the total number of bookings for each vehicle and display only those vehicles that have -- more than 2 bookings.
 
 
 SELECT
